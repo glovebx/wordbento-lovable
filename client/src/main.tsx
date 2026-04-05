834e8312f943f321c797095a5e9cbb3441a6781d
glovebx <glovebx@glovebxdeMacBook-Pro-2.local>
2026-04-05 13:27:35 +0800

    login bug fix; remove verbose AuthProvider node

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AuthProvider } from '@/contexts/AuthContext';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 if(import.meta.env.NODE_ENV != 'development') {
@@ -11,8 +10,6 @@ if(import.meta.env.NODE_ENV != 'development') {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </StrictMode>,
 )
